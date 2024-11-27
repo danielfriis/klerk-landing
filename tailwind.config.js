cf3d3801c744cf7533db7f0295b37d1de3f0de4f
Daniel Friis <d@friis.me>
2024-11-27 11:07:36 +0000

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,21 @@ module.exports = {
       'sans': [...defaultTheme.fontFamily.sans],
       'serif': [...defaultTheme.fontFamily.serif]
     },
+    colors: {
+      'westar': {
+        '50': '#f7f6f5',
+        '100': '#e4e0dc',
+        '200': '#d9d3cf',
+        '300': '#c1b8b0',
+        '400': '#a8988f',
+        '500': '#968379',
+        '600': '#89746d',
+        '700': '#73605b',
+        '800': '#5f514d',
+        '900': '#4e4340',
+        '950': '#292221',
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/typography')
